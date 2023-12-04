4708893676b097c51fd53e7ae93f1fa2f8f4a252
AYAN10M <gopalchandrahaldar1997@gmail.com>
2023-12-04 20:18:23 +0530

    DOCUMENT OBJECT MODEL STARTED

--- PRATICE/JAVASCRIPT_PRATICE/sample1/javascript/script.js
+++ PRATICE/JAVASCRIPT_PRATICE/sample1/javascript/script.js
@@ -1,2 +1,28 @@
-// document.getElementById("id_name"); : tells the browser to find the element with the id of "id_name" 
-document.getElementById("count_people").innerText = 5;
+var username = prompt("Enter your name:");
+var message = "Welcome, " + username + "!" + " I am alexa and I am here to help you.";
+alert(message);
+
+
+// Algorithm to solve the follwing problem :-
+//      1. Create a global variable to store the count
+//      2. Listen for clicks on the increment button
+//      3. Increment the count variable when the button is clicked
+//      4. Change the count-el in the HTML to reflect the new count
+
+
+// using a global variable
+let count = 0;
+console.log(count);
+
+function increment() {
+    count = count + 1;
+    document.getElementById("count_people").innerText = count;
+}
+
+// 1. Create a function, save(), which logs out the count when it's called 
+
+function save()
+{
+    console.log(count);
+}
+
